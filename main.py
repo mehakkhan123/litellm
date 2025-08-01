2fb3bc926fe1b23c32935c514f301f7ca1a00f01
mehakkhan123 <mahekkhan284@gmail.com>
2025-08-01 17:23:40 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from litellm import completion
 import os
-def main():
-    print("Hello from litellm-project!")
+from dotenv import load_dotenv
 
+load_dotenv()
+
+
+def main():
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    response = completion(
+        model="gemini/gemini-2.0-flash",
+        messages=[
+            {
+                "role":"user",
+                "content":"Who is the founder of Pakistan?"
+            }
+        ],
+    )
+
+    print(response['choices'][0]['message']['content'])
 
 if __name__ == "__main__":
     main()
